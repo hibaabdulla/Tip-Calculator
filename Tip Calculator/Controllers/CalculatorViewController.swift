6940809b6d7840fd41bf987ae442e98ea4f677dd
Hiba Abdulla <hibaabdullahkk@gmail.com>
2025-05-25 14:50:53 -0700

    Finish Tip Calculator

--- Tip Calculator/Controllers/CalculatorViewController.swift
+++ Tip Calculator/Controllers/CalculatorViewController.swift
@@ -11,28 +11,50 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var zeroPCButton: UIButton!
     @IBOutlet weak var billTextField: UITextField!
-    
     @IBOutlet weak var twentyPCButton: UIButton!
     @IBOutlet weak var tenPCButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    var total: Double = 0
-    var percentage = 0
-    
-    
+    var percentage: Double = 0
+    var billAmount: Double = 0
+    var selectedTip = 10
+    var numberOfSplits = 0
+    var result:Double = 0
+        
     @IBAction func tipButtonPressed(_ sender: UIButton) {
+        zeroPCButton.isSelected = false
+        tenPCButton.isSelected = false
+        twentyPCButton.isSelected = false
+        
+        sender.isSelected = true
+        selectedTip = sender.tag
     }
     
     @IBAction func calculateButton(_ sender: UIButton) {
         
-        
+        if let bill = billTextField.text, let number = Double(bill) {
+            billAmount = number
+        }
+            
+        percentage = Double(selectedTip) / 100
+        result = (billAmount + (billAmount*percentage)) / Double(numberOfSplits)
         
         self.performSegue(withIdentifier: "goToResult", sender: self)
-        
-        
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.total = result
+            destinationVC.splits = numberOfSplits
+            destinationVC.tip = selectedTip
+        }
+    }
+   
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        numberOfSplits = Int(sender.value)
+        splitNumberLabel.text = String(numberOfSplits)
+        
     }
 }
 
--- Tip Calculator/Controllers/ResultViewController.swift
+++ Tip Calculator/Controllers/ResultViewController.swift
@@ -8,16 +8,23 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    
+    var total:Double = 0
+    var splits = 0
+    var tip = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        totalAmount.text = String(total)
+        descriptionLabel.text = "Split between \(splits) people, with \(tip)% tip."
     }
     
 
     @IBAction func recalculateButton(_ sender: UIButton) {
-        
         self.dismiss(animated: true)
     }
     
